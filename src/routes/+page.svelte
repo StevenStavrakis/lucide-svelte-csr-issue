faf60474a9a1a16d6d0097a59fa7974ae246708c
Steven Stavrakis <s.a.stavrakis@gmail.com>
2023-12-24 10:59:18 -0500

    created example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
+<script lang="ts">
+  import { ChevronDown } from "lucide-svelte";
+
+  let showIcon = $state(false);
+
+  $effect(() => {
+    setTimeout(() => {
+      showIcon = true;
+    }, 1000);
+  });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<p>Server Rendered Icon</p>
+<ChevronDown size="64" />
+{#if showIcon}
+  <p>Browser Rendered Icon</p>
+  <ChevronDown size="64" />
+{/if}
